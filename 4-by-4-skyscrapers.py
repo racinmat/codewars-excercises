244096c2670ad650ac18eca670fd85fc1a6fa04c
Matěj Račinský <racinsky.matej@seznam.cz>
2019-02-16 23:13:44 +0100

    init

---
+++ 4-by-4-skyscrapers.py
@@ -0,0 +1,14 @@
+def solve_skyscrapers(size):
+    # todo: implement me
+    pass
+
+
+def main():
+    size = 4
+    clues = [0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0]
+    assert len(clues) == size ** 2
+    solve_skyscrapers(clues)
+
+
+if __name__ == '__main__':
+    main()
